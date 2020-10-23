b9cc52f74b5427c6ef577cc939ca6f032a891003
mamoot <nicolas.perussel@gmail.com>
2020-10-23 21:01:44 +0200

    chore(stock): add increase and decrease ressources to deal with stock management.

--- src/Resources/StockManagement/StockResource.php
+++ src/Resources/StockManagement/StockResource.php
@@ -41,4 +41,51 @@ final class StockResource extends HttpCaller
     {
         return $this->get(sprintf('/stock/file?idGame=%d&isSealed=%s&idLanguage=%d', $gameId, $isSealed, $idLanguage));
     }
+
+    /**
+     * Increase stock for the given article.
+     *
+     * @param int $articleId
+     * @param int $stock
+     *
+     * @return array
+     *      The Article object into array. (https://api.cardmarket.com/ws/documentation/API_2.0:Entities:Article)
+     * @throws \Exception
+     */
+    public function increaseStock(int $articleId, int $stock): array
+    {
+        return $this->put('/stock/increase', $this->createArticle($articleId, $stock));
+    }
+
+    /**
+     * Decrease stock for the given article.
+     *
+     * @param int $articleId
+     * @param int $stock
+     *
+     * @return array
+     *      The Article object into array. (https://api.cardmarket.com/ws/documentation/API_2.0:Entities:Article)
+     * @throws \Exception
+     */
+    public function decreaseStock(int $articleId, int $stock): array
+    {
+        return $this->put('/stock/decrease', $this->createArticle($articleId, $stock));
+    }
+
+    /**
+     * Dedicated definition of the Article Object to deal with stock management.
+     * (https://api.cardmarket.com/ws/documentation/API_2.0:Entities:Article)
+     *
+     * @param int $articleId
+     * @param int $stock
+     *
+     * @return array
+     */
+    private function createArticle(int $articleId, int $stock): array
+    {
+        return [
+          "idArticle" => $articleId,
+          "amount" => $stock,
+        ];
+    }
 }
