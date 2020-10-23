349c4c6bb63c37e6f3f4e5185ba507250c0d0df9
mamoot <nicolas.perussel@gmail.com>
2020-10-23 21:01:44 +0200

    chore(stock): add increase and decrease ressources to deal with stock management.

--- src/Resources/HttpCaller.php
+++ src/Resources/HttpCaller.php
@@ -40,6 +40,8 @@ abstract class HttpCaller
     }
 
     /**
+     * Perform GET request.
+     *
      * @param string $uri
      *
      * @return array
@@ -55,6 +57,34 @@ abstract class HttpCaller
         try {
             $response = $this->httpClient->request('GET', $url, [
               'headers' => self::getAuthorizationHeader($url, 'GET'),
+            ]);
+
+            return self::processJsonResponse($response);
+        } catch (UnknownErrorException | DecodingExceptionInterface | HttpExceptionInterface | TransportExceptionInterface $exception) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * Perform PUT request.
+     *
+     * @param string $uri
+     * @param array $content
+     *
+     * @return array
+     * @throws \Mamoot\CardMarket\Exception\UnknownErrorException
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    protected function put(string $uri, array $content): array
+    {
+        $url = HttpClientCreator::API_URL . $uri;
+
+        try {
+            $response = $this->httpClient->request('PUT', $url, [
+              'headers' => self::getAuthorizationHeader($url, 'PUT'),
+              'body' => json_encode($content),
             ]);
 
             return self::processJsonResponse($response);
